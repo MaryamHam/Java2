51d81bb60e05b4696286dc8d99a5391d52ece193
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-08-06 19:40:16 +0400

    To calculate triangle area using OOP

---
+++ polymorphism/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle(5,3);
+        System.out.println(" the area of triangle = "+ triangle.getArea());
+    }
+}
+
